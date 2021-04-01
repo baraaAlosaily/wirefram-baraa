c0824e70ce162069e671d937837a3e561501e39d
baraaAlosaily <seelbaraa@gmail.com>
2021-04-01 14:12:36 +0300

    lab3

---
+++ index.js
@@ -0,0 +1,28 @@
+var useName = prompt('Welcome to banana website please enter ASAC', 'ASAC');
+alert = useName;
+
+var sumOf = prompt('Enter sum of 2 and 4', '6'),
+    alert = sumOf;
+
+var baNana = prompt('please enter number of banana names you know')
+alert = baNana;
+
+if (baNana < 0) {
+    document.write('Good');
+}
+else if (baNana >= 0) {
+    document.write('V.good');
+}
+else {
+    document.write('Enter to know more about banana');
+}
+
+var welcome = prompt("welcome to our web site,(Type 10 to visit us agin)", 10);
+alert = welcome;
+
+if (welcome == 10) {
+    document.write("visit us again");
+}
+else {
+    document.write("Try to visit us again");
+}
