b491de9269e6420be5aa35f5da64b7eab71086b4
baraaAlosaily <seelbaraa@gmail.com>
2021-04-05 19:16:27 +0300

    bata

--- new.js
+++ new.js
@@ -1,48 +1,75 @@
-// var useName = prompt('Welcome to banana website please enter ASAC', 'ASAC');
-// alert = useName;
 
-// var sumOf = prompt('Enter sum of 2 and 4', '6'),
-//     alert = sumOf;
 
-// var useName = prompt('Welcome to banana website please enter ASAC', 'ASAC');
-// alert = useName;
+var userName = prompt('Welcome to banana website please enter ASAC', 'ASAC');
+var sumOf = prompt('Enter sum of 2 and 4', '6');
+var baNana = prompt('please enter number of banana names you know');
 
-// var sumOf = prompt('Enter sum of 2 and 4', '6'),
-//     alert = sumOf;
+function banNa() {
 
-// var baNana = prompt('please enter number of banana names you know')
-// alert = baNana;
-
-// if (baNana < 0) {
-//     document.write('Good');
-// }
-// else if (baNana >= 0) {
-//     document.write('V.good');
-// }
-// else {
-//     document.write('Enter to know more about banana');
-// }
-
-// var bananaType = prompt("please enter the type of banana");
-// while (bananaType !== 'appleBanana' && bananaType !== 'redBanana') {
-//     bananaType = prompt("Please choose appleBanana or redBanana");
-// }
-// var numberOfbanana = prompt("please enter the number of banana");
-// for (var i = 0; i < numberOfbanana; i++) {
-//     if (bananaType == 'appleBanana') {
-//         document.write('<img src="https://i.ytimg.com/vi/K8YsfVZxavk/maxresdefault.jpg"/>');
-//     }
-//     else {
-//         document.write('<img src="https://sc04.alicdn.com/kf/UTB86qkLlrPJXKJkSafSq6yqUXXaQ.jpg"/>');
-//     }
-// }
-
-// for ( i = 1 to 5 ) {
-//     console.log(i);
-//   }
-
-var i = 0;
-while (i <= 10) {
-    console.log(i);
-    i++;
+    userName = prompt('Welcome to banana website please enter ASAC', 'ASAC');
+    sumOf = prompt('Enter sum of 2 and 4', '6');
+    baNana = prompt('please enter number of banana names you know');
 }
+banNa();
+
+
+function banaNa2() {
+
+    if (baNana < 0) {
+        document.write('Good');
+    }
+    else if (baNana >= 0) {
+        document.write('V.good');
+    }
+    else {
+        document.write('Enter to know more about banana');
+    }
+
+}
+
+banaNa2();
+
+
+
+var bananaType;
+
+function newNumber() {
+
+    while (bananaType !== 'appleBanana' && bananaType !== 'redBanana') {
+        var bananaType = prompt("Please choose appleBanana or redBanana");
+    }
+
+}
+
+newNumber();
+
+var numberOfbanana = prompt("please enter the number of banana");
+
+function BarAa() {
+    for (var i = 0; i < numberOfbanana; i++) {
+        if (bananaType == 'appleBanana') {
+            document.write('<img src="https://i.ytimg.com/vi/K8YsfVZxavk/maxresdefault.jpg"/>');
+        }
+        else {
+            document.write('<img src="https://sc04.alicdn.com/kf/UTB86qkLlrPJXKJkSafSq6yqUXXaQ.jpg"/>');
+        }
+    }
+}
+BarAa();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
